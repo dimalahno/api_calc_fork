17bbd44664652644430c62c2a2e98eba0236e067
dimalahno <dimalahno@rambler.ru>
2026-02-26 14:46:53 +0500

    fix router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -5,8 +5,11 @@ from fastapi import APIRouter
 from app.api.v1.calculate import router as calculate_router
 from app.api.v1.health import router as health_router
 from app.api.v1.reference import router as reference_router
+from app.core.config import settings
+
+
 
 router = APIRouter()
-router.include_router(health_router)
-router.include_router(reference_router)
-router.include_router(calculate_router)
+router.include_router(health_router, prefix=settings.API_PREFIX)
+router.include_router(reference_router, prefix=settings.API_PREFIX)
+router.include_router(calculate_router, prefix=settings.API_PREFIX)
--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-import os
-from dataclasses import dataclass
 from functools import lru_cache
 from pathlib import Path
-from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -14,29 +12,6 @@ model_config = SettingsConfigDict(
     env_file_encoding="utf-8"
 )
 
-# @dataclass(frozen=True)
-# class Settings:
-#     api_title: str
-#     api_version: str
-#     reference_file_path: str
-#     APP_HOST: str
-#     APP_PORT: int
-#     DEBUG_MODE: bool
-
-# @lru_cache
-# def get_settings() -> Settings:
-#     default_reference_path = str(
-#         Path(__file__).resolve().parents[2] / "справочник_УК_обновленный_2025_06_07_1.txt"
-#     )
-#     return Settings(
-#         api_title=os.getenv("API_TITLE", "Punishment API"),
-#         api_version=os.getenv("API_VERSION", "0.1.0"),
-#         reference_file_path=os.getenv("REFERENCE_FILE_PATH", default_reference_path),
-#         APP_HOST=os.getenv("APP_HOST", "127.0.0.1"),
-#         APP_PORT=int(os.getenv("APP_PORT", 8000)),
-#         DEBUG_MODE=os.getenv("DEBUG_MODE", "True").lower() in ("true", "1", "yes"),
-#     )
-
 class Settings(BaseSettings):
     api_title: str = "Punishment API"
     api_version: str = "0.1.0"
@@ -44,8 +19,9 @@ class Settings(BaseSettings):
         Path(__file__).resolve().parents[2] / "справочник_УК_обновленный_2025_06_07_1.txt"
     )
     APP_HOST: str = "127.0.0.1"
-    APP_PORT: int = 8000
+    APP_PORT: int = 9000
     DEBUG_MODE: bool = True
+    API_PREFIX: str = "/api/v1"
 
     model_config = SettingsConfigDict(
         env_file=str(BASE_DIR / ".env"),
@@ -55,3 +31,5 @@ class Settings(BaseSettings):
 @lru_cache
 def get_settings() -> Settings:
     return Settings()
+
+settings = get_settings()
